3665617383143f57966bf3162c04ea995883135c
Marko Boras <mboras1@etfos.hr>
2021-11-02 09:42:56 +0100

    add NewMeetupForm

--- src/pages/NewMeetup.tsx
+++ src/pages/NewMeetup.tsx
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 const NewMeetup: React.FC = () => {
-  return <div>NewMeetup Page</div>;
+  return (
+    <section>
+      <h1>Add new Meetup</h1>
+      <NewMeetupForm></NewMeetupForm>
+    </section>
+  );
 };
 
 export default NewMeetup;
