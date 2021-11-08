fd689cdb4441bb1832e58939d8365381cb3ae7d8
Marko <mboras1@etfos.hr>
2021-11-08 18:13:44 +0100

    remove all-meetups context

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { FavoritesContextProvider } from 'store/FavoritesContext';
-import { MeetupsContextProvider } from 'store/AllMeetupsContext';
 import './index.css';
 import { App } from 'App';
 import { Provider } from 'react-redux';
@@ -10,13 +9,11 @@ import { store } from './store/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <MeetupsContextProvider>
-      <FavoritesContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </FavoritesContextProvider>
-    </MeetupsContextProvider>
+    <FavoritesContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritesContextProvider>
   </Provider>,
   document.getElementById('root'),
 );
